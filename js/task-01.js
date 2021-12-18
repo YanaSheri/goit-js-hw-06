e98e8243dcd86ac3bd6b6bb984e793271fd8aeba
YanaSheri <yana.shereheda@gmail.com>
2021-12-19 00:00:32 +0200

    Update task -01

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,14 @@
+const categories = document.querySelector("#categories");
+const numberOfCategories = categories.children.length;
+console.log(`Number of categories: ${numberOfCategories}`);
+
+const arrLi = categories.children;
+[...arrLi].forEach(el => {
+    console.log(`Category: ${el.firstElementChild.textContent}`);
+    console.log(`Elements: ${el.lastElementChild.children.length}`);
+});
+
+
 // --- 1 ЗАДАНИЕ I ВАРИАНТ ---
 // const categories = document.querySelectorAll("#categories .item");
 // console.dir(categories);
@@ -9,11 +20,13 @@
 // const numberOfCategories = categories.children.length;
 // console.log(`Number of categories: ${numberOfCategories}`);
 
-// --- ОБА ЗАДАНИЯ ---
-const arrItems = document.querySelectorAll("li.item");
-const numberOfCategories = arrItems.length;
-console.log(`Number of categories: ${numberOfCategories}`);
-const newArr = arrItems.forEach(item => {
-    console.log(`Category: ${item.childNodes[1].textContent}`);
-    console.log(`Elements: ${item.childNodes[3].children.length}`);
-});
+// --- III ВАРИАНТ ---
+// const arrItems = document.querySelectorAll("li.item");
+// const numberOfCategories = arrItems.length;
+// console.log(`Number of categories: ${numberOfCategories}`);
+// const newArr = arrItems.forEach(item => {
+//     console.log(`Category: ${item.childNodes[1].textContent}`);
+//     console.log(`Elements: ${item.childNodes[3].children.length}`);
+// });
+
+
