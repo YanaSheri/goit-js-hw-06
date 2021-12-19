8ec242c267b7d665bd04b4bc995d805c7096825f
YanaSheri <yana.shereheda@gmail.com>
2021-12-19 21:54:40 +0200

    Update task-03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,61 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+// console.log(item);
+// let listItem;
+// let imageEl;
+// images.forEach(image => {
+  // listItem = document.createElement('li');
+  // console.log(listItem);
+  // // image = document.createElement("img");
+  // // console.log(image);
+  // listItem.append(document.createElement("img"));
+
+  // const url = image.url;
+  // console.log(url);
+  // const alt = image.alt;
+  // console.log(alt);
+
+//   imageEl = document.createElement("img");
+//   imageEl.url = image.url;
+//   console.log(imageEl);
+
+//   listItem = document.createElement('li');
+//   // console.log(listItem);
+//   // 
+//   // console.log(image);
+//   // listItem.append(document.createElement("img"));
+// });
+
+// const list = document.querySelector('.gallery');
+// list.insertAdjacentHTML('afterbegin', listItem);
+
+
+// const list = document.querySelector(".gallery");
+// const elements = images.map(image => {
+//   const imageEl = document.createElement("img");
+//   // console.log(image.url);
+//   imageEl.src = image.url;
+//   imageEl.alt = image.alt;
+//   console.log(imageEl);
+//   const item = document.createElement("li");
+//   // item.append(elements);
+// });
+// console.log(elements);
+// const item = document.createElement("li");
+// item.append(elements);
+// list.append(item);
+// list.insertAdjacentHTML('afterbegin', ...elements);
+// // list.append(createElement("li")  ...elements);
+
+
+
+const list = document.querySelector(".gallery");
+const markup = images
+  .map(image => `<li><img class="item" src="${image.url}" alt="${image.alt}" width="200"></li>`)
+  .join("");
+list.insertAdjacentHTML("beforeend", markup);
+list.style.listStyle = "none";
+console.log(list);
