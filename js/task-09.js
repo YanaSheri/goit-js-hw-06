e72c436f7a07545baa48a8383d6ec49a38eb3b87
YanaSheri <yana.shereheda@gmail.com>
2021-12-20 19:46:42 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.body;
+
+const cb = (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+};
+
+btn.addEventListener('click', cb);
