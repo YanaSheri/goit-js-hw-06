67a06f3750ff615a9e9d571a68c54c7a62c8af88
YanaSheri <yana.shereheda@gmail.com>
2021-12-21 01:07:17 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,27 +7,15 @@ const ingredients = [
   'Condiments',
 ];
 
-// console.log(ingredients);
 const list = document.querySelector('#ingredients');
-let listItem = [];
-// ingredients.forEach(item => {
-//   listItem = document.createElement("li");
-//   console.log(listItem);
-//   listItem.textContent = item;
-//   listItem.classList.add('item');
-//   list.append(listItem);
-// });
+let listItem;
 
 const products = ingredients.map(ingredient => {
   listItem = document.createElement("li");
   console.log(listItem);
   listItem.textContent = ingredient;
   listItem.classList.add('item');
-  // listItem.push(ingredient);
+  return listItem;
 });
 
-
-
-console.log(products);
-list.append(products);
-console.log(list);
+list.append(...products);
