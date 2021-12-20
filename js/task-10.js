72748000e5b640f23370b524178e7316e8abe5a2
YanaSheri <yana.shereheda@gmail.com>
2021-12-20 20:06:48 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const refs = {
+  input: document.querySelector('#controls').children[0],
+  btn1: document.querySelector('[data-create'),
+  btn2: document.querySelector('[data-destroy'),
+}
+
+console.log(refs.input);
